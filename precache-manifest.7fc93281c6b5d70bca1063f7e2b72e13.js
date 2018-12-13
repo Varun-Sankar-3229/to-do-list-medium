158f9e8a3c21e52b47b379f0b47c8602633cc953
unknown <varunitssankar@gmail.com>
2018-12-13 17:30:44 +0530

    Updates

---
+++ precache-manifest.7fc93281c6b5d70bca1063f7e2b72e13.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "74bb1eef505e79f7c999",
+    "url": "/to-do-list-medium/static/js/runtime~main.74bb1eef.js"
+  },
+  {
+    "revision": "6bd6d20525507d642757",
+    "url": "/to-do-list-medium/static/js/main.6bd6d205.chunk.js"
+  },
+  {
+    "revision": "7bb667f2b3ef66fbc898",
+    "url": "/to-do-list-medium/static/js/1.7bb667f2.chunk.js"
+  },
+  {
+    "revision": "6bd6d20525507d642757",
+    "url": "/to-do-list-medium/static/css/main.7eac97ce.chunk.css"
+  },
+  {
+    "revision": "aa22cc81ef16bf5a0217836c243d2894",
+    "url": "/to-do-list-medium/index.html"
+  }
+];
